77322418f090f1a4799192d614238035708f01d0
GuiFreittas <125324600+GuiFreittas@users.noreply.github.com>
2023-08-17 23:40:16 -0300

    lista_de_numeros.py

--- lista_de_numeros.py
+++ lista_de_numeros.py
@@ -1,7 +1,1 @@
-def calcular_media(lista):
-    media = sum(lista) / len(lista)
-    return media
 
-numeros = [10, 20, 30, 40, 50]
-media = calcular_media(numeros)
-print("A média dos números na lista é:", media)
